0f4038a5b1366e04ce7440e6bc13cc94cc73a94d
SinjulMSBH <sinjul.msbh@yahoo.com>
2019-07-17 07:36:45 +0430

    Add project files.

---
+++ Simple/.vshistory/Startup.cs/2019-07-17_06_55_42_154.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Simple.Data;
+
+using WebEssentials.AspNetCore.Pwa;
+
+namespace Simple
+{
+	public class Startup
+	{
+		private const string _baseURL = "/PWAApp";
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+			});
+
+			services.AddHttpContextAccessor();
+
+			//services.AddWebAppManifest();
+			//services.AddServiceWorker();
+			//services.AddProgressiveWebApp();
+
+			services.AddProgressiveWebApp(new PwaOptions
+			{
+				BaseRoute = _baseURL
+				//CacheId = "v1.1::CacheFirstSafe",
+				//OfflineRoute = "/Offline",
+				//RoutesToPreCache = "/, /contact.html, data.json",
+				//Strategy = ServiceWorkerStrategy.CacheFirst,
+			});
+
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(
+					Configuration.GetConnectionString("DefaultConnection")));
+			services.AddDefaultIdentity<IdentityUser>()
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+
+			services.AddControllersWithViews();
+			services.AddRazorPages();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseCookiePolicy();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapRazorPages();
+			});
+		}
+	}
+}
